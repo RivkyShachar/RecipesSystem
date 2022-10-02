c5eea3f7a74511c97dae3c3e1ec8307d5102659c
David <David@DESKTOP-J2PMT7Q>
2022-10-02 04:01:26 +0300

    started to make the weather functions

---
+++ BL/WeatherLogic.cs
@@ -0,0 +1,27 @@
+﻿using DP;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BL
+{
+    public class WeatherLogic
+    {
+        public string GetWeather(string City)
+        {
+            DAL.WeatherAdapter dal = new DAL.WeatherAdapter();
+            Root myWeather = null;
+            string myJson = dal.GetWeather(City);
+            if (myJson != null)
+            {
+                myWeather = JsonConvert.DeserializeObject<Root>(myJson);
+            }
+            foreach (var item in myWeather.items)
+            {
+                var a = item;
+            }
+            return "";
+        }
+    }
+}
---
+++ RecipesSystem.GetwayServer/Controllers/ImaggaController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RecipesSystem.GetwayServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImaggaController : ControllerBase
+    {
+        // GET: api/<ImaggaController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<ImaggaController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<ImaggaController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ImaggaController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ImaggaController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
