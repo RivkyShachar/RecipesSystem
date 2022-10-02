195a662d520ae09aa52c74e177fb859ff71bb559
David <David@DESKTOP-J2PMT7Q>
2022-10-02 04:25:01 +0300

    add few new empty classes

---
+++ DAL/USDAadapter.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAL
+{
+    internal class USDAadapter
+    {
+        //https://api.nal.usda.gov/fdc/v1/foods/search?query=apple&pageSize=2&api_key=iuHCuFBxp4hhEOZyWA4RYLPvXjrOfqfd8q6J9yLo
+        //iuHCuFBxp4hhEOZyWA4RYLPvXjrOfqfd8q6J9yLo
+    }
+}
---
+++ RecipesSystem.GetwayServer/Controllers/USDAController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace RecipesSystem.GetwayServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class USDAController : ControllerBase
+    {
+        // GET: api/<USDAController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<USDAController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<USDAController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<USDAController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<USDAController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
