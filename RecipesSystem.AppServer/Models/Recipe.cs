7810e2a7cbf64b5ade3fe9224854b1e295c5fe29
David <David@DESKTOP-J2PMT7Q>
2022-11-03 08:29:04 +0200

    add error mesages to add new recipe need to make it red

--- RecipesSystem.AppServer/Models/Recipe.cs
+++ RecipesSystem.AppServer/Models/Recipe.cs
@@ -12,7 +12,7 @@ namespace RecipesSystem.AppServer.Models
     {
         //need to add more error maegag
         public int Id { get; set; }
-        [Required]
+        [Required(ErrorMessage = "Required field")]
         [StringLength(50,ErrorMessage = "Name cannot be longer than 50 characters.")]
         public string Name { get; set; }
 
@@ -20,26 +20,36 @@ namespace RecipesSystem.AppServer.Models
         public string Description { get; set; } = "";
 
         [DisplayName("Preparation instructions")]
-        public string PrepInstructions { get; set; } = "";
+        [Required(ErrorMessage = "Required field")]
+        [MinLength(10, ErrorMessage ="Instructions cannot be shorter than 10 characters.")]
+        public string PrepInstructions { get; set; }
 
         [DisplayName("Ingredients")]
-        public string Ingredients  { get; set; }//Ingredients
+        [Required(ErrorMessage = "Required field")]
+        [MinLength(10, ErrorMessage = "Ingredients cannot be shorter than 10 characters.")]
+        public string Ingredients  { get; set; }
 
         [DisplayName("Image URL")]
+        [Required(ErrorMessage = "Required field")]
         public string ImageURL { get; set; }
 
         [DisplayName("preparation time")]
+        [Required(ErrorMessage = "Required field")]
         [StringLength(10, ErrorMessage = "Making time cannot be longer than 10 characters.")]
-        public string TimeToMake { get; set; }//Time To Make
+        public string TimeToMake { get; set; }
 
         [DisplayName(" Cooking Time")]
+        [Required(ErrorMessage = "Required field")]
         [StringLength(10, ErrorMessage = "Cooking time cannot be longer than 10 characters.")]
         public string  CookingTime { get; set; }
 
         [DisplayName("Diners size")]
-        public int Diners { get; set; }//כמה סועדים neet to add an error if its a minus 
+        [Required(ErrorMessage = "Required field")]
+        [Range(1, 1000, ErrorMessage = "Value for {0} must be between {1} and {2}.")]
+        public int Diners { get; set; }
 
         [DisplayName("Tags")]
+        [Required(ErrorMessage = "Required field")]
         public Tags Tag { get; set; }
 
         [DisplayName("Write Note")]
@@ -47,8 +57,10 @@ namespace RecipesSystem.AppServer.Models
 
         public string Rate { get; set; } ="";//rate of the recipe
 
+        [Required(ErrorMessage = "Required field")]
         public Holidays Holiday { get; set; } = Holidays.NOTHOLIDAY;//the date of the rating
 
+        [Required(ErrorMessage = "Required field")]
         public Weathers Weather { get; set; } = Weathers.NICE;//the weather in the rating
 
     }
